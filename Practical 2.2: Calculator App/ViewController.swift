e7366a2126d22ab1e1e7de052be03d8807c9f144
Avishka Perera <avi.v.perera@icloud.com>
2018-06-07 07:58:41 +1000

    final commit

--- Practical 2.2: Calculator App/ViewController.swift
+++ Practical 2.2: Calculator App/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var X: UITextField!
+    @IBOutlet weak var Y: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var History: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,35 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calcOps(_ sender: UIButton) {
+        
+        var value : Double = 0;
+        switch sender.tag {
+            
+        case 0:
+            value = Double(X.text!)! + Double(Y.text!)!
+            History.text = "\(History.text!) \n  \(X.text!) + \(X.text!) = \(value)"
+            
+        case 1:
+            value = Double(X.text!)! - Double(Y.text!)!
+            History.text = "\(History.text!) \n  \(X.text!) - \(X.text!) = \(value)"
+            
+        case 2:
+            value = Double(X.text!)! * Double(Y.text!)!
+            History.text = "\(History.text!) \n  \(X.text!) * \(X.text!) = \(value)"
+            
+        case 3:
+            value = Double(X.text!)! / Double(Y.text!)!
+            History.text = "\(History.text!) \n  \(X.text!) / \(X.text!) = \(value)"
+            
+        default:
+            print ("error")
+        }
+        
+        
+        result.text = "Result: " + "\n \(value)"
+    }
+    
+    
 }
 
